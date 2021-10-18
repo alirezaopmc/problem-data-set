b3e6762d86dcca0c8734e9b606a9086f5cfa239d
shahab <61025096+shahabsteam@users.noreply.github.com>
2021-10-19 00:11:35 +0330

    Update 1.sh

--- problems/1.sh
+++ problems/1.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-command="ls";
-eval "$command";
 FILES="./in/*"
 for f in $FILES
 do
   #echo "Processing $f file..."
-  # take action on each file. $f store current file name
+
   out=$(eval "echo $f | grep -Eo [0-9]+")
 
   eval "python3 solution.py <$f | diff  -c out/output$out.txt -  "
