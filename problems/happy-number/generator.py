a48fac0e225ceacab962cfd5455b365f089d5f96
shahabsteam <shahabsteam@gmail.com>
2021-10-20 21:56:13 +0330

    added happy number solution

---
+++ problems/happy-number/generator.py
@@ -0,0 +1,105 @@
+import random
+import math
+
+# Utility functions
+def arrayToString(arr: list):
+    result: str = ""
+
+    for i in range(len(arr)):
+        if i + 1 < len(arr):
+            result += "{} ".format(arr[i])
+        else:
+            result += "{}".format(arr[i])
+    
+    return result
+    
+
+rnd = lambda minimum=0, maximum=10**9: random.randint(minimum, maximum)
+
+
+count = 0
+helper = None
+
+def generate(i): 
+    ans =[1, 7, 10, 13, 19, 23, 28, 31, 32, 44, 49, 68, 70, 79, 82, 86, 91, 94, 97, 100, 103, 109, 129, 130, 133, 139, 167, 176, 188, 190, 192, 193, 203, 208, 219, 226, 230, 236, 239, 262, 263, 280, 291, 293, 301, 302, 310, 313, 319, 320, 326, 329, 331, 338, 356, 362, 365, 367, 368, 376, 379, 383, 386, 391, 392, 397, 404, 409, 440, 446, 464, 469, 478, 487, 490, 496, 536, 556, 563, 565, 566, 608, 617, 622, 623, 632, 635, 637, 638, 644, 649, 653, 655, 656, 665, 671, 673, 680, 683, 694, 700, 709, 716, 736, 739, 748, 761, 763, 784, 790, 793, 802, 806, 818, 820, 833, 836, 847, 860, 863, 874, 881, 888, 899, 901, 904, 907, 910, 912, 913, 921, 923, 931, 932, 937, 940, 946, 964, 970, 973, 989, 998, 1000 ]
+    out=False
+    n = random.randint(1,2)
+    if(n==1):
+        n = random.randint(0,142)
+        n = ans[n]
+        p = str(n)
+        p+="0"*random.randint(1,5)
+        return int(p),True
+    return random.randint(2**10,2**16),out
+
+def makeInputs(inString, cnt):
+    path = "./in/input" + str(cnt) + ".txt"
+    f = open(path, "w")
+    f.write(inString)
+    f.close()
+
+def makeOutputs(outString, cnt):
+    path = "./out/output" + str(cnt) + ".txt"
+    f = open(path, "w")
+    f.write(outString)
+    f.close()
+
+# Manually add some inputs
+if "Case 0":
+    n = 1
+    
+    inString = "{}\n".format(
+        n
+    )
+
+    outString = "True\n"
+    
+    count += 1
+    makeInputs(inString, count)
+    makeOutputs(outString, count)
+    
+if "Case 2":
+    n = 9130000
+    
+    inString = "{}\n".format(
+        n
+    )
+
+    outString = "True\n"
+    
+    count += 1
+    makeInputs(inString, count)
+    makeOutputs(outString, count)
+if "Case 3":
+    n=1418832
+    inString = "{}\n".format(
+        n
+    )
+    outString = "False\n"
+    count += 1
+    makeInputs(inString, count)
+    makeOutputs(outString, count)
+    
+    
+# Automatically add inputs
+for i in range(47):
+    print(i)
+    inputs, outputs = generate(i)
+
+    n = inputs
+    ans = outputs
+
+    inString = "{}\n".format(
+      n
+    )
+
+    outString = "{}\n".format(
+       ans
+    )
+
+    count += 1
+    makeInputs(inString, count)
+    makeOutputs(outString, count)
+
+
+print("Completed!")
